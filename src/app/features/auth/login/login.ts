0985bc4b885949594cd001e8291f6a9d02b424d4
Raghda <raghdalloukh@gmail.com>
2026-08-01 22:38:17 +0100

    Correction erreur de syntaxe dans login.ts

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -17,18 +17,20 @@ export class Login {
   isLoading = false;
 
   constructor(private authService: AuthService, private router: Router) {}
-onSubmit() {
-  this.errorMessage = '';
-  this.isLoading = true;
 
-  this.authService.login(this.email, this.password).subscribe({
-    next: () => {
-      this.isLoading = false;
-      this.router.navigate(['/dashboard']);
-    },
-    error: (err) => {
-      this.isLoading = false;
-      this.errorMessage = err.error?.error || 'Une erreur est survenue';
-    },
-  });
+  onSubmit() {
+    this.errorMessage = '';
+    this.isLoading = true;
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        this.isLoading = false;
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.error || 'Une erreur est survenue';
+      },
+    });
+  }
 }
